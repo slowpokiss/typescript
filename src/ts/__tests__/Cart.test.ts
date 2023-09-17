3ca114a72e19e973dcdcf39dee76bfa13d364640
Michel <hltvexe@mail.ru>
2023-09-18 01:06:48 +0300

    added logic for 2nd task

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -6,9 +6,40 @@ import { resolvePlugin } from '@babel/core';
 
 test('add all 3', () => {
   const cart = new Cart();
-  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new Book(1007, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
   cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
-  cart.add(new Movie('Avengers', 2012, 'USA', 'Avengers Assemble!', ['sci-fi', 'action', 'fantasy', 'adventure'], 137));
-  const result = cart.items.length
+  cart.add(new Movie(1009,'Avengers', 8000, 2012, 'USA', 'Avengers Assemble!', ['sci-fi', 'action', 'fantasy', 'adventure'], 137));
+  const result = cart.items.length;
   expect(result).toBe(3);
+});
+
+
+test('remove cart', () => {
+  const cart = new Cart();
+  cart.add(new Book(1007, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Movie(1009,'Avengers', 8000, 2012, 'USA', 'Avengers Assemble!', ['sci-fi', 'action', 'fantasy', 'adventure'], 137));
+  cart.remove(1008);
+
+  expect(cart.items.length).toStrictEqual(2);
+});
+
+test('get price', () => {
+  const cart = new Cart();
+  cart.add(new Book(1007, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Movie(1009,'Avengers', 8000, 2012, 'USA', 'Avengers Assemble!', ['sci-fi', 'action', 'fantasy', 'adventure'], 137));
+  const result = cart.returnPrice();
+
+  expect(result).toBe(10900);
+});
+
+test('get price with discount', () => {
+  const cart = new Cart();
+  cart.add(new Book(1007, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Movie(1009,'Avengers', 8000, 2012, 'USA', 'Avengers Assemble!', ['sci-fi', 'action', 'fantasy', 'adventure'], 137));
+  const result = cart.retPriceDiscount(20);
+
+  expect(result).toBe(8720);
 });
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -10,4 +10,17 @@ export default class Cart {
     get items(): Buyable[] {
         return [...this._items]; 
     }
+
+    remove(id: number): Buyable[] {
+        this._items = this._items.filter((el) => el.id !== id)
+        return [...this._items]
+    }
+
+    returnPrice(): number { 
+        return this._items.reduce((acc, el) => acc + el.price, 0)
+    }
+
+    retPriceDiscount(discount: number): number {
+        return this._items.reduce((acc, el) => acc + el.price, 0) * (1 - discount / 100) 
+    }
 }
